0873753482c27d877024263a0644e42048b44c06
Maxim <maxim271989@gmail.com>
2024-11-28 19:20:22 +0300

    added images

--- components/beauty/OfferHeader.tsx
+++ components/beauty/OfferHeader.tsx
@@ -12,17 +12,17 @@ export function OfferHeader() {
           Акция "СИЯЙ"
         </Badge>
         <Badge variant="secondary" className="bg-pink-100">
-          <Gift className="w-4 h-4 mr-1" />
-          С сюрприз-подарком
+          <Gift className="w-4 h-4 mr-1" />С сюрприз-подарком
         </Badge>
       </div>
       <h1 className="text-4xl font-bold text-gray-900 bg-clip-text text-transparent bg-gradient-to-r from-pink-600 to-purple-600">
         Профессиональный уход за лицом
       </h1>
       <div className="max-w-2xl mx-auto p-6 border-2 border-pink-200 rounded-[20px] bg-white/50 backdrop-blur-sm shadow-lg">
-        <p className="text-gray-600 mt-6">
-          Дорогие Девочки!!! В преддверии Новогоднего праздника, хочу предложить Вам свою Акцию "СИЯЙ" 
-          (с сюрпризом подарком!!!) За чисто символическую цену (с выездом на дом).
+        <p className="text-gray-600 text-2xl">
+          Дорогие Девочки!!! В преддверии Новогоднего праздника, хочу предложить
+          Вам свою Акцию "СИЯЙ" (с сюрпризом подарком!!!) За чисто символическую
+          цену (с выездом на дом).
         </p>
       </div>
     </div>
